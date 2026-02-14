384430cfddc12add9bdacdf6c29a7caf83935bde
jakeflavin <jake.flavin@sngular.com>
2026-02-14 06:16:18 -0500

    chore: initial portfolio app (coming soon)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div style={{ 
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "system-ui"
+    }}>
+      <h1>Coming Soon</h1>
+    </div>
+  );
+}
